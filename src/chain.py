b0633197036f0e6407bd371517d13fd52127b5a5
Darius90332 <89436430+Darius90332@users.noreply.github.com>
2025-04-15 14:50:07 -0400

    implementation partial chaining algorithm

--- src/chain.py
+++ src/chain.py
@@ -1,3 +1,31 @@
 """Configure a hash table using a chain approach."""
 
 # TODO: implement chain approach
+
+class HashTableChainning:
+    """ A hash table implementation using separate chaining."""
+
+    def  __init__(self, capacity):
+        self.capacity = capacity
+        self.buckets = [[] for _ in range(capacity)]
+        self.collisions = 0
+
+    def put(self, key, value):
+        """Inserts the key-value pair into the hash table.
+        A collison is recorded if the target bucket is not empty.
+        if the key already exists in the bucket, its value is updated.
+        """
+
+        index = hash(key) % self.capacity
+        bucket = self.buckets[index]
+        # Record a collison if there is already at least one element in the bucket.
+        if bucket:
+            self.collisions += 1
+            # Update the value if the key already exists.
+            for i, (k, v) in enumerate(bucket):
+                if k == key:
+                    bucket[i] = (key, value)
+                    return
+            bucket.append((key, value))
+
+    
